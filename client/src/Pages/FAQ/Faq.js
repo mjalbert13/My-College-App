d52196a3cb973247c2d8a4cd6fe28e735e0b707c
mjalbert13 <mjalbert13@gmail.com>
2019-08-15 20:30:09 -0400

    created a  boilerplate FAQ page

---
+++ client/src/Pages/FAQ/Faq.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+
+class FaqPage extends Component {
+
+    render(){
+        return (
+            <div className = "container">
+                <div class="jumbotron jumbotron-fluid">
+                    <div class="container">
+                        <h1 class="display-4">Need Help?</h1>
+                         <p class="lead">Here are some of the most asked question</p>
+                    </div>
+                </div>
+                <div className="constainer">
+                    <h3>What college should I go to? </h3>
+                    <p>I don't know that why you are here!</p>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default FaqPage
