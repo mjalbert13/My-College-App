d507f947cd6c255da73b42fb560c17a5d09a34da
mjalbert13 <mjalbert13@gmail.com>
2019-08-17 12:07:24 -0400

    added backend passport files and

--- server.js
+++ server.js
@@ -2,17 +2,35 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const MongoStore = require('connect-mongo') (session)
+const session = require('express-session');
+const passport = require('passport')
+const user = require('./server/routes/usersRoutes');
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(morgan('dev'));
+
+app.use(session({
+  secret: "College",
+  store: MongoStore({}),
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
 // Define API routes here
-
+app.use('/users', user);
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("*", (req, res) => {
