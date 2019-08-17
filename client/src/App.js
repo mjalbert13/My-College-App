7c5e18c20f87d7faa35b0fa619d0049a8e57260b
Eastwo0d7 <46980901+Eastwo0d7@users.noreply.github.com>
2019-08-17 13:21:05 -0400

    Home branch (#27)
    * styled vine border
    
    * updating
    
    * updating from master

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,11 @@
 import React, { Component } from "react";
 import Home from "./pages/Home/Home";
-import logo from "./logo.svg";
-import "./App.css";
-import FaqPage from './Pages/FAQ/Faq'
-
 
 class App extends Component {
   render (){
     return (
-
-      
-      <div className="App">
+      <div className="Home">
         <Home />
-
       </div>
     )
 
