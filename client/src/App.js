ffee416bee64a12b2d60208f583e073e784811fb
mjalbert13 <47984960+mjalbert13@users.noreply.github.com>
2019-08-20 20:18:31 -0400

    moved passport files into client (#31)

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
 
-import FaqPage from './Pages/FAQ/Faq'
-import Navbar from "./components/Navbar";
-import SearchPage from "./Pages/Search/Search";
-import Home from "./pages/Home/Home";
+import Register from './Components/searchForms/register'
 
 class App extends Component {
   render (){
     return (
       <div className="Home">
-        <Home />
-        {/* <Navbar /> */}
-        {/* <SearchPage /> */}
+        <Register />
 
       </div>
     )
