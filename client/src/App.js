e3db84db08b5da60eb900512eb3a3db6525d001b
Mike <mjeanjacques90@gmail.com>
2019-08-20 20:19:45 -0400

    searh page fixed spacing and errors

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,18 @@
 import React, { Component } from "react";
 import "./App.css";
 
-import FaqPage from './Pages/FAQ/Faq'
-import Navbar from "./components/Navbar";
-import SearchPage from "./Pages/Search/Search";
-import Home from "./pages/Home/Home";
+// import FaqPage from './Pages/FAQ/Faq'
+// import Navbar from "./components/Navbar";
+// import SearchPage from "./Pages/Search/Search";
+import Home from "./pages/Home/Home.js";
 
 class App extends Component {
   render (){
     return (
       <div className="Home">
         <Home />
-        {/* <Navbar /> */}
-        {/* <SearchPage /> */}
-
       </div>
-    )
-
+    );
   }
-
 }
 export default App;
