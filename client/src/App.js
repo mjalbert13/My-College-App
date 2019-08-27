1db0610489ee177d9419d63ca5a81108376a2fc5
Brett LeBaron <brett.lebaron@gmail.com>
2019-08-26 21:24:36 -0400

    init branch commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,18 @@
 import React, {Component} from 'react';
 // import Navbar from "./components/Navbar";
-
-// import SearchPage from "./Pages/Search/Search";
-import Home from "./pages/Home/Home.js";
-import Register from './Components/searchForms/register'
-
-
-// import Navbar from "./components/Navbar";
-// import SearchPage from "./pages/Search/Search";
-import Home from "./pages/Home/Home.js";
-
+import SearchPage from "./pages/Search/Search";
+// import Home from "./pages/Home/Home.js";
 // import Register from './Components/searchForms/register'
 
 class App extends Component {
   render (){
     return (
 
-
-      
-
      <div className="App">
-        <Register />
+        {/* <Home /> */}
+        {/* <Navbar /> */}
+        <SearchPage />
+        {/* <Register /> */}
 
      </div>
       
