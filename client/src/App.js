44d19b7f1ea6b2cc380b89e8fbd4d630c19fe6d3
Patrick Mahon <patrickmahon@Patricks-MacBook-Pro.local>
2019-09-07 13:56:26 -0400

    fixed saved component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import FaqPage from './pages/FAQ/Faq';
-import Search from "./pages/Search/Search";
+import FaqPage from './Pages/FAQ/Faq';
+import Search from "./Pages/Search/Search";
 import Register from './Components/Register/register';
-import Home from "./pages/Home/Home";
-import Saved from "./pages/Saved/Saved";
+import Home from "./Pages/Home/Home";
+import Saved from "./Pages/Saved/Saved";
 import Login from "./Components/Login/Login"
 import PageNav from './Components/pageNav';
 import axios from 'axios'
